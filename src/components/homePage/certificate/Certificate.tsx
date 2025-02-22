85f17d8df24c7d4e15d7ee77bfb009491b5b94bc
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-02-22 21:29:01 +0200

    added certificate, deleted unnecessary images

--- src/components/homePage/certificate/Certificate.tsx
+++ src/components/homePage/certificate/Certificate.tsx
@@ -1,5 +1,17 @@
 import React from "react";
 
 export default function Certificate() {
-  return <div>Certificate</div>;
+  return (
+    <section
+      id="certificate"
+      className="container max-w-[1920px] scroll-mt-[82px] tabxl:scroll-mt-[124px] py-[60px] laptop:py-[93px] mb-[123px] laptop:mb-14"
+    >
+      <h2 className="mb-5 font-michelin uppercase italic text-center text-24bold tabxl:text-40bold">
+        Наш сертификат
+      </h2>
+      <p className="font-michelin uppercase italic text-center text-20bold tabxl:text-24bold text-transparent bg-clip-text bg-purpleGradient">
+        подтверждение профессионализма
+      </p>
+    </section>
+  );
 }
