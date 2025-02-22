90c8899665b0124342a3ec94d06fc20154da4e32
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-02-22 23:07:14 +0200

    added cta texts

--- src/components/homePage/ctaSignUp/CtaSignUp.tsx
+++ src/components/homePage/ctaSignUp/CtaSignUp.tsx
@@ -22,7 +22,15 @@ export default function CtaSignUp() {
           className="absolute -bottom-[30px] left-[30px] w-[calc(100%-60px)]"
         />
       </div>
-      <div className="container max-w-[1920px]"></div>
+      <div className="container max-w-[1920px] mt-[103px] tabxl:mt-0">
+        <h2 className="mb-4 tabxl:mb-8 font-michelin uppercase italic text-24bold tabxl:text-36bold text-transparent bg-clip-text bg-pinkGradient">
+          Присоединяйся сейчас и меняй свою жизнь!
+        </h2>
+        <p className="mb-7 tabxl:mb-10 text-14light tabxl:text-20light">
+          Подпишись на наш Telegram и начни зарабатывать на трейдинге уже
+          сегодня. Готовые стратегии, разбор сделок и обучение – всё бесплатно!
+        </p>
+      </div>
     </section>
   );
 }
