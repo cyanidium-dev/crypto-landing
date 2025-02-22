46cb1d57f20f2bd32416f64221ca7f5f8df336f4
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-02-22 23:36:05 +0200

    added cta for mob

--- src/components/homePage/ctaSignUp/CtaSignUp.tsx
+++ src/components/homePage/ctaSignUp/CtaSignUp.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import Image from "next/image";
+import MainButton from "@/components/shared/buttons/MainButton";
+import { TELEGRAM_URL } from "@/constants/constants";
 
 export default function CtaSignUp() {
   return (
     <section
-      className="scroll-mt-[82px] tabxl:scroll-mt-[124px] pt-[30px] laptop:pt-[145px] pb-14 laptop:pb-[131px] 
+      className="tabxl:flex flex-row-reverse scroll-mt-[82px] tabxl:scroll-mt-[124px] pt-[30px] laptop:pt-[145px] pb-14 laptop:pb-[131px] 
   mb-[73px] laptop:mb-[188px]"
     >
-      <div className="relative pl-2">
+      <div className="relative max-w-[375px] pl-2 mx-auto">
         <Image
           src="/images/contentImages/ctaSignUpMan.webp"
           alt="man in the sunglasses"
@@ -23,13 +25,24 @@ export default function CtaSignUp() {
         />
       </div>
       <div className="container max-w-[1920px] mt-[103px] tabxl:mt-0">
-        <h2 className="mb-4 tabxl:mb-8 font-michelin uppercase italic text-24bold tabxl:text-36bold text-transparent bg-clip-text bg-pinkGradient">
-          Присоединяйся сейчас и меняй свою жизнь!
-        </h2>
-        <p className="mb-7 tabxl:mb-10 text-14light tabxl:text-20light">
-          Подпишись на наш Telegram и начни зарабатывать на трейдинге уже
-          сегодня. Готовые стратегии, разбор сделок и обучение – всё бесплатно!
-        </p>
+        <div className="max-w-[682px] mx-auto">
+          <h2 className="mb-4 tabxl:mb-8 font-michelin uppercase italic text-24bold tabxl:text-36bold text-transparent bg-clip-text bg-pinkGradient">
+            Присоединяйся сейчас и меняй свою жизнь!
+          </h2>
+          <p className="mb-7 tabxl:mb-10 text-14light tabxl:text-20light">
+            Подпишись на наш Telegram и начни зарабатывать на трейдинге уже
+            сегодня. Готовые стратегии, разбор сделок и обучение – всё
+            бесплатно!
+          </p>
+          <a
+            href={TELEGRAM_URL}
+            target="_blank"
+            rel="noopener noreferrer nofollow"
+            className="block w-full tabxl:w-[196px] max-w-[331px] mx-auto tabxl:mx-0"
+          >
+            <MainButton>Подписаться</MainButton>
+          </a>
+        </div>
       </div>
     </section>
   );
