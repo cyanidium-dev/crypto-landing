bae14186453696df764eba9e53f8f0eb37ac01ea
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-02-23 11:42:55 +0200

    added before section title and description

--- src/components/homePage/before/Before.tsx
+++ src/components/homePage/before/Before.tsx
@@ -1,5 +1,17 @@
 import React from "react";
 
 export default function Before() {
-  return <div>Before</div>;
+  return (
+    <section
+      className="container max-w-[1920px] scroll-mt-[82px] tabxl:scroll-mt-[124px] pt-[100px] tabxl:pt-[91px] pb-14 tabxl:pb-[125px] 
+    mb-[78px] tabxl:mb-[127px]"
+    >
+      <h2 className="font-michelin uppercase text-center tabxl:text-left text-24bold tabxl:text-40bold">
+        Моё «До» и «После» в трейдинге
+      </h2>
+      <p className="text-14light tabxl:text-20light">
+        Вот как может выглядеть ваш путь от новичка до успешного трейдера.
+      </p>
+    </section>
+  );
 }
