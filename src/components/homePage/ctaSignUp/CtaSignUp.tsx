257c39a8122a2c48e4b4222dfe91c186c9eb7e80
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-02-23 11:26:13 +0200

    added cta sign up

--- src/components/homePage/ctaSignUp/CtaSignUp.tsx
+++ src/components/homePage/ctaSignUp/CtaSignUp.tsx
@@ -6,27 +6,28 @@ import { TELEGRAM_URL } from "@/constants/constants";
 export default function CtaSignUp() {
   return (
     <section
-      className="tabxl:flex flex-row-reverse scroll-mt-[82px] tabxl:scroll-mt-[124px] pt-[30px] laptop:pt-[145px] pb-14 laptop:pb-[131px] 
+      className="container max-w-[1920px] tabxl:flex tabxl:flex-row-reverse tabxl:items-center tabxl:justify-between scroll-mt-[82px] tabxl:scroll-mt-[124px] pt-[30px] laptop:pt-[145px] pb-14 laptop:pb-[131px] 
   mb-[73px] laptop:mb-[188px]"
     >
-      <div className="relative max-w-[375px] pl-2 mx-auto">
+      <div className="relative max-w-[375px] tabxl:max-w-[420px] laptop:max-w-[514px] desk:max-w-[540px] deskxl:max-w-[700px] mx-auto tabxl:mx-0">
         <Image
           src="/images/contentImages/ctaSignUpMan.webp"
           alt="man in the sunglasses"
           width={1028}
           height={1232}
+          className=""
         />
         <Image
           src="/images/contentImages/startEarning.svg"
           alt="start earning text"
           width="316"
           height="175"
-          className="absolute -bottom-[30px] left-[30px] w-[calc(100%-60px)]"
+          className="absolute -bottom-[30px] tabxl:-bottom-3 left-4 tabxl:left-10 w-[90%]"
         />
       </div>
-      <div className="container max-w-[1920px] mt-[103px] tabxl:mt-0">
-        <div className="max-w-[682px] mx-auto">
-          <h2 className="mb-4 tabxl:mb-8 font-michelin uppercase italic text-24bold tabxl:text-36bold text-transparent bg-clip-text bg-pinkGradient">
+      <div className="tabxl:w-[51.6%] laptop:w-[41.9%] desk:w-[50%] deskxl:w-[45%] mt-[103px] tabxl:mt-0">
+        <div className="max-w-[682px] desk:max-w-full mx-auto">
+          <h2 className="pr-2 mb-4 tabxl:mb-8 font-michelin uppercase text-24bold tabxl:text-36bold text-transparent bg-clip-text bg-pinkGradient">
             Присоединяйся сейчас и меняй свою жизнь!
           </h2>
           <p className="mb-7 tabxl:mb-10 text-14light tabxl:text-20light">
--- src/components/homePage/tradingBenefits/TradingBenefits.tsx
+++ src/components/homePage/tradingBenefits/TradingBenefits.tsx
@@ -8,11 +8,11 @@ export default function TradingBenefits() {
       className="container max-w-[1920px] scroll-mt-[82px] tabxl:scroll-mt-[124px] pt-[65px] laptop:pt-[145px] pb-[63px] laptop:pb-[131px] 
       mb-[73px] laptop:mb-[188px]"
     >
-      <h2 className="mb-4 tabxl:mb-5 font-michelin uppercase italic text-20bold tabxl:text-40bold text-center">
+      <h2 className="mb-4 tabxl:mb-5 font-michelin uppercase text-20bold tabxl:text-40bold text-center">
         Плюсы трейдинга
       </h2>
       <p
-        className="w-fit pr-2 mx-auto mb-[50px] tabxl:mb-[68px] font-michelin uppercase italic text-24bold tabxl:text-32bold text-transparent 
+        className="w-fit pr-2 mx-auto mb-[50px] tabxl:mb-[68px] font-michelin uppercase text-24bold tabxl:text-32bold text-transparent 
       bg-clip-text bg-caviarGradient text-center"
       >
         Почему это выгодно?
