f5ef6a2873512ef12f3c8e89480c50769aad9793
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-02-24 12:09:40 +0200

    added cta image

--- src/components/homePage/ctaWebinar/OpenTelegramChat.tsx
+++ src/components/homePage/ctaWebinar/OpenTelegramChat.tsx
@@ -4,7 +4,7 @@ import CountdownTimer from "./CountdownTimer";
 
 export default function OpenTelegramChat() {
   return (
-    <div className="max-w-[682px] tabxl:max-w-[1100px] py-[44px] tabxl:py-[35px] px-[35px] mx-auto mt-14 tabxl:mt-16 rounded-[26px] bg-caviarGradient">
+    <div className="max-w-[682px] tabxl:max-w-full py-[44px] tabxl:py-[35px] px-[35px] mx-auto mt-14 tabxl:mt-16 rounded-[26px] bg-caviarGradient">
       <h3 className="max-w-[347px] tabxl:max-w-[488px] mx-auto mb-4 font-michelin uppercase text-16bold tabxl:text-24bold text-center">
         Открыли канал на 24 часа для первых 100 зашедших пользователей
       </h3>
