401fd982661f30f902c51adcee8c5958c033b227
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-02-25 19:56:46 +0200

    added backgrounds for reviews section

--- src/components/homePage/reviews/Reviews.tsx
+++ src/components/homePage/reviews/Reviews.tsx
@@ -1,23 +1,17 @@
 import React from "react";
-import Image from "next/image";
 import ReviewsSlider from "./ReviewsSlider";
+import ReviewsImages from "./ReviewsImages";
 
 export default function Reviews() {
   return (
     <section
       id="reviews"
-      className="relative w-full pt-[430px] tabxl:pt-[154px] pb-[55px] tabxl:pb-[124px] mb-[75px] tabxl:mb-[10px] overflow-hidden"
+      className="relative w-full pt-[430px] tabxl:pt-[154px] pb-[55px] tabxl:pb-[124px] mb-[75px] tabxl:mb-[10px] overflow-x-clip"
     >
-      <Image
-        src="/images/contentImages/reviewsMan.webp"
-        alt="man"
-        width={1024}
-        height={768}
-        className="absolute -z-10 -top-4 tabxl:top-[137px] left-[calc(50%-322px+12px)] tabxl:-left-[90px] w-[644px] max-w-[644px] h-[483px] object-cover origin-center overflow-visible"
-      />
+      <ReviewsImages />
       <div className="container max-w-[1920px] tabxl:ml-[35.5%] laptop:ml-[30.5%] mb-[61px] tabxl:mb-[71px]">
         <h2
-          className="w-fit mb-3 font-michelin uppercase text-32bold tabxl:text-64bold text-transparent bg-clip-text bg-caviarGradient text-center 
+          className="w-fit pr-1 mx-auto tabxl:mx-0 mb-3 font-michelin uppercase text-32bold tabxl:text-64bold text-transparent bg-clip-text bg-caviarGradient text-center 
       tabxl:text-left"
         >
           Отзывы
