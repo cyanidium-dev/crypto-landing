f9d68a2380b084e6184d3224485f6706d7ff4342
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-02-25 20:50:34 +0200

    added one row dollar background to the rviews section

--- src/components/homePage/reviews/ReviewsImages.tsx
+++ src/components/homePage/reviews/ReviewsImages.tsx
@@ -11,6 +11,13 @@ export default function ReviewsImages() {
         height={768}
         className="absolute -z-20 -top-4 tabxl:top-[137px] left-[calc(50%-322px+12px)] tabxl:-left-[90px] w-[644px] max-w-[644px] h-[483px] object-cover origin-center overflow-visible"
       />
+      <Image
+        src="/images/backgrounds/oneRowDollarsDesk.webp"
+        alt="man"
+        width={1920}
+        height={154}
+        className="hidden tabxl:block absolute z-10 top-[131px] tabxl:left-[79%] laptop:left-[69%] h-[154px] object-cover object-left overflow-visible"
+      />
       <div className="absolute -z-30 top-[162px] tabxl:top-[315px] right-[calc(50%-478px)] tabxl:right-auto tabxl:left-[38px] w-[649px] h-[747px] rounded-full bg-purple blur-[150px]"></div>
       <div className="absolute -z-10 top-[188px] tabxl:top-[315px] right-[calc(50%-436px)] tabxl:right-auto tabxl:left-[-215px] w-[748px] tab:w-[1048px] tabxl:w-[1307px] h-[696px] tabxl:h-[1216px] rounded-full bg-black blur-[85px] tabxl:blur-[150px]"></div>
     </>
--- src/components/homePage/tradingBenefits/TradingBenefitsImages.tsx
+++ src/components/homePage/tradingBenefits/TradingBenefitsImages.tsx
@@ -1,11 +1,25 @@
+import Image from "next/image";
 import React from "react";
 
 export default function TradingBenefitsImages() {
   return (
     <>
+      <Image
+        src="/images/backgrounds/tradingBenefitsSection/dollarMob.webp"
+        alt="dollars"
+        width="1024"
+        height="1027"
+        className="block tabxl:hidden absolute -z-50 top-[10px] left-0 h-[1027px] object-cover object-left"
+      />
+      {/* <Image
+        src="/images/backgrounds/afterSection/dollarDesk.webp"
+        alt="dollars"
+        fill
+        className="hidden tabxl:block absolute -z-50 top-0 left-0 object-cover"
+      /> */}
       <div className="absolute -z-20 top-[-73px] left-0 w-full h-[759px] tabxl:h-[829px] bg-afterSectionTopGradient"></div>
       <div className="absolute -z-30 left-0 bottom-0 tabxl:bottom-[-167px] w-full h-[266px] tabxl:h-[312px] bg-bottomGradient"></div>
-      <div className="absolute z-10 left-[15px] tabxl:left-[460px] top-[220px] w-[226px] h-[188px] rounded-full bg-caviarGradient tabxl:blur-[165px]"></div>
+      <div className="absolute z-10 left-[15px] tabxl:left-[460px] top-[220px] w-[226px] h-[188px] rounded-full bg-caviarGradient blur-[165px]"></div>
     </>
   );
 }
