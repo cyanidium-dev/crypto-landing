1ce8f40c5d9805bcd6b65dfa96a0b4d02c08c98e
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-02-25 20:57:19 +0200

    added dollars bg to the trading benefits section

--- src/components/homePage/tradingBenefits/TradingBenefitsImages.tsx
+++ src/components/homePage/tradingBenefits/TradingBenefitsImages.tsx
@@ -11,12 +11,13 @@ export default function TradingBenefitsImages() {
         height="1027"
         className="block tabxl:hidden absolute -z-50 top-[10px] left-0 h-[1027px] object-cover object-left"
       />
-      {/* <Image
-        src="/images/backgrounds/afterSection/dollarDesk.webp"
+      <Image
+        src="/images/backgrounds/tradingBenefitsSection/dollarDesk.webp"
         alt="dollars"
-        fill
-        className="hidden tabxl:block absolute -z-50 top-0 left-0 object-cover"
-      /> */}
+        width="1920"
+        height="676"
+        className="hidden tabxl:block absolute -z-50 top-4 left-0 h-[676px] object-cover object-left"
+      />
       <div className="absolute -z-20 top-[-73px] left-0 w-full h-[759px] tabxl:h-[829px] bg-afterSectionTopGradient"></div>
       <div className="absolute -z-30 left-0 bottom-0 tabxl:bottom-[-167px] w-full h-[266px] tabxl:h-[312px] bg-bottomGradient"></div>
       <div className="absolute z-10 left-[15px] tabxl:left-[460px] top-[220px] w-[226px] h-[188px] rounded-full bg-caviarGradient blur-[165px]"></div>
