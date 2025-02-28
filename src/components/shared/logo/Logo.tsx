7f3e61e81b68970d2ea3ac8dd526d3ff39565376
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-02-28 11:21:43 +0200

    added hover, focus, active for logo

--- src/components/shared/logo/Logo.tsx
+++ src/components/shared/logo/Logo.tsx
@@ -9,7 +9,8 @@ export default function Logo({ className = "" }: LogoProps) {
   return (
     <Link href="/" className="flex justify-center">
       <p
-        className={`pr-1 overflow-visible font-michelin uppercase text-transparent bg-clip-text bg-logoGradient ${className}`}
+        className={`pr-1 overflow-visible font-michelin uppercase text-transparent bg-clip-text bg-logoGradient active:scale-[99%] 
+          focus-visible:brightness-[125%] laptop:hover:brightness-[125%] transition duration-300 ease-out outline-none ${className}`}
       >
         Нурлан о трейдинге
       </p>
